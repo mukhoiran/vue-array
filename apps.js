ea97f51e76ba72ebc85c3979dd1b8758ae420cdd
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-08-10 09:53:42 +0700

    show all array value

--- apps.js
+++ apps.js
@@ -2,6 +2,11 @@ var app = new Vue({
   el:'#app',
 
   // <!-- =========== show 1 array value =========== -->
+  // data:{
+  //   vehicle:['car','motorcycle','bike']
+  // }
+
+  // <!-- =========== show all array value =========== -->
   data:{
     vehicle:['car','motorcycle','bike']
   }
